ed609b78d6fdafdbcdd6e05ee7a70a1505b658b6
Rafe Khatebi <rkhatibi2003@gmail.com>
2026-05-04 20:44:50 +0430

    worked the admin pages

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -36,20 +36,6 @@
                     <a href="{{ route('admin.dashboard') }}" class="navbar-brand mx-4 mb-3">
                         <h3 class="text-primary"><i class="fa fa-map-marked-alt me-2"></i>Makanyab</h3>
                     </a>
-                    <div class="d-flex align-items-center ms-4 mb-4">
-                        <div class="position-relative">
-                            <img class="rounded-circle" src="{{ asset('dashmin/img/user.jpg') }}" alt="User"
-                                style="width: 40px; height: 40px;">
-                            <div
-                                class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
-                            </div>
-                        </div>
-                        <div class="ms-3">
-                            <h6 class="mb-0">{{ auth()->user()->name }}</h6>
-                            <span>{{ ucfirst(auth()->user()->role) }}</span>
-                        </div>
-                    </div>
-
                     <div class="navbar-nav w-100">
                         <a href="{{ route('admin.dashboard') }}"
                             class="nav-item nav-link {{ request()->routeIs('admin.dashboard') ? 'active' : '' }}">
@@ -122,13 +108,9 @@
                     <div class="navbar-nav align-items-center ms-auto">
                         <div class="nav-item dropdown">
                             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                                <img class="rounded-circle me-lg-2" src="{{ asset('dashmin/img/user.jpg') }}"
-                                    alt="User" style="width: 40px; height: 40px;">
                                 <span class="d-none d-lg-inline-flex">{{ auth()->user()->name }}</span>
                             </a>
-                            <div
-                                class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
-                                <a href="{{ route('home') }}" class="dropdown-item">Visit Site</a>
+                            <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                                 <form method="POST" action="{{ route('logout') }}">
                                     @csrf
                                     <button type="submit" class="dropdown-item">Log Out</button>
