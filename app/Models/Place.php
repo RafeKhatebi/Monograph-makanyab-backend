100dd9ce5a836081998c36b500bea53937d94f93
Rafe Khatebi <rkhatibi2003@gmail.com>
2026-05-12 20:37:36 +0700

    updated the place model

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\q2HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
